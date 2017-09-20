dac4c076c115e4bad4867693232443e8e4854846
xabiugarte <xabiugarte@users.noreply.github.com>
2017-09-20 10:55:59 -0400

    Fixed issue with documentation

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['.docs_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['.docs_build', 'Thumbs.db', '.DS_Store', "qemu", "volatility"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -154,4 +154,10 @@ texinfo_documents = [
 ]
 
 
+def skip(app, what, name, obj, skip, options):
+    if name == "__init__":
+        return False
+    return skip
 
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
